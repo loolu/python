3d7d1fa49c55cce0f89ae5d868ad19d5a50f489c
loolu <88646039@qq.com>
2018-09-06 19:41:58 +0800

    20180906更新类文件方法

--- pythonclasstwo.py
+++ pythonclasstwo.py
@@ -17,5 +17,8 @@ class Dog():
 
 my_dog = Dog('willie', 6)
 
+my_dog.sit()
+my_dog.roll_over()
+
 print("My dog's name is " + my_dog.name.title() + ".")
 print("My dog is " + str(my_dog.age) + " years old.")
